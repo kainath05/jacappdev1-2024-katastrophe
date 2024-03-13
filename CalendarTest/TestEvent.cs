2fd5bd6ec7436e6e2c98f381891182e0da55f031
kainath Ahmed <2268774@johnabbottcollege.net>
2024-03-13 10:59:48 -0400

    Added new test in event

--- CalendarTest/TestEvent.cs
+++ CalendarTest/TestEvent.cs
@@ -48,19 +48,18 @@ namespace CalendarCodeTests
 
             // Act
             Event copy = new Event(Event);
-            //copy.DurationInMinutes = Event.DurationInMinutes + 15; //CANNOT WRITE ONTO IT ANYMORE
 
             // Assert 
             Assert.Equal(id, Event.Id);
             Assert.NotEqual(DurationInMinutes, copy.DurationInMinutes);
-            Assert.Equal(Event.DurationInMinutes + 15, copy.DurationInMinutes);
+            Assert.Equal(Event.DurationInMinutes, copy.DurationInMinutes);
             Assert.Equal(descr, Event.Details);
             Assert.Equal(category, Event.Category);
             Assert.Equal(now, Event.StartDateTime);
         }
 
         [Fact]
-        public void EventObject_PropertiesAreReadOnly()
+        public void EventObject_PropertiesAreReadOnly() //New test
         {
             // Arrange
             DateTime now = DateTime.Now;
@@ -83,36 +82,6 @@ namespace CalendarCodeTests
 
 
         // ========================================================================
-
-        [Fact]
-        public void EventObjectGetSetProperties()
-        {
-            // question - why cannot I not change the date of an Event.  What if I got the date wrong?
-
-            // Arrange
-            DateTime now = DateTime.Now;
-            double DurationInMinutes = 24.55;
-            string descr = "New Sweater";
-            int category = 34;
-            int id = 42;
-            double newDurationInMinutes = 54.55;
-            string newDescr = "Angora Sweater";
-            int newCategory = 38;
-
-            Event Event = new Event(id, now, category, DurationInMinutes, descr);
-
-            // Act
-            Event.DurationInMinutes = newDurationInMinutes;
-            Event.Category = newCategory;
-            Event.Details = newDescr;
-
-            // Assert 
-            Assert.True(typeof(Event).GetProperty("StartDateTime").CanWrite == false);
-            Assert.True(typeof(Event).GetProperty("Id").CanWrite == false);
-            Assert.Equal(newDurationInMinutes, Event.DurationInMinutes);
-            Assert.Equal(newDescr, Event.Details);
-            Assert.Equal(newCategory, Event.Category);
-        }
 
 
     }
