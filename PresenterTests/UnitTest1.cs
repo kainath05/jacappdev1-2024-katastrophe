a4c92df21514019097ff04b3657cad41b7b52e19
kainath Ahmed <2268774@johnabbottcollege.net>
2024-04-17 12:05:48 -0400

    made test testing constructor

--- PresenterTests/UnitTest1.cs
+++ PresenterTests/UnitTest1.cs
@@ -1,11 +1,35 @@
+using Calendar;
 namespace PresenterTests
 {
+    public class TestView : View
+    {
+        public bool messageWorks;
+        
+        public bool ConfirmCloseApplication()
+        {
+            return true;
+        }
+
+        public void ShowMessage(string message)
+        {
+            messageWorks = true;
+        }
+    }
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestConstructor()
         {
+            TestView view = new TestView();
+            Presenter p = new Presenter(view);
+            Assert.IsType<Presenter>(p);
+        }
 
+        [Fact]
+        public void TestDatabase()
+        {
+            TestView view = new TestView();
+            Presenter p = new Presenter(view);
         }
     }
 }
